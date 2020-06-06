f89f3ec07b64d5af5d64cf4d0cc70511b8a3cb5f
Pedro <citadel153@hotmail.com>
2020-06-06 14:49:50 +0200

    Modifico script de base de datos y modelo E-R

--- BBDD/pochadia.sql
+++ BBDD/pochadia.sql
@@ -6,11 +6,11 @@ CREATE TABLE usuario (
   correo varchar(50) NOT NULL,
   estado ENUM('Conectado','Ausente','Ocupado') NOT NULL,
   imagen varchar(40), /*¿NOT NULL?*/ 
-  suscrito ENUM('SI','NO') NOT NULL,
+  suscrito BIT NOT NULL,
   PRIMARY KEY (id_usuario)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO usuario VALUES('00001','Pedro','1234','1994/10/11','correo@hotmail.com','Conectado','perfil.jpg','00001');
+INSERT INTO usuario VALUES('00001','Pedro','1234','1994/10/11','correo@hotmail.com','Conectado','perfil.jpg',1);
 
 CREATE TABLE juego (
   id_juego INT(5) NOT NULL,
@@ -21,51 +21,105 @@ CREATE TABLE juego (
   puntuacion_5_S INT(5) NOT NULL,/*5 estrellas*/
   titulo varchar(50) NOT NULL,
   autor varchar(50) NOT NULL,
-  genero varchar(300), /*¿ENUM?*/
   anyo_lanzamiento INT(4) NOT NULL,
   tipo_suscriptor ENUM('NORMAL','PREMIUM') NOT NULL,
   franquicia varchar(50) NOT NULL,
   idiomas varchar(500) NOT NULL,
-  pegi varchar(50) NOT NULL,/*¿ENUM?*/
-  general_horas_jugadas INT(20) NOT NULL,
   fecha_subida date NOT NULL,
   imagen varchar(40) NOT NULL,
-  disponible ENUM('SI','NO') NOT NULL, 
+  disponible BIT NOT NULL, 
   multijugador ENUM('NO','LOCAL','EN LINEA') NOT NULL,
   num_jugadores INT(3) NOT NULL,
   PRIMARY KEY (id_juego)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO juego VALUES('00001','12','5','34','0','2','Assassins Creed Odyssey','Ubisoft','Acción, Aventura',2019,'NORMAL','Assassin\'s Creed','Inglés, francés, italiano, alemán, español, japonés, coreano, neerlandés, polaco, portugués, ruso, chino (simplificado), chino (tradicional)','18',389,'2020/06/02','Assassins_Creed_Odyssey.jpg','SI','NO',1);
+INSERT INTO juego VALUES('00001','12','5','34','0','2','Assassins Creed Odyssey','Ubisoft',2019,'NORMAL','Assassins Creed','Inglés, francés, italiano, alemán, español, japonés, coreano, neerlandés, polaco, portugués, ruso, chino (simplificado), chino (tradicional)','2020/06/02','Assassins_Creed_Odyssey.jpg',1,'NO',1);
 
 CREATE TABLE comentario (
   id_comentario INT(5) NOT NULL,
   id_usuario INT(5) NOT NULL,
-  nombre_usuario varchar(20) NOT NULL,
   votos_positivos INT(10) NOT NULL,
   votos_negativos INT(10) NOT NULL,
-  hora time NOT NULL,
+  hora_fecha datetime NOT NULL,
   cabecera varchar(50) NOT NULL,
   texto varchar(600) NOT NULL,
-  horas_jugadas_user INT(10) NOT NULL,
-  recomendado ENUM('SI','NO'),
+  recomendado BIT NOT NULL,
   img_usuario varchar(40) NOT NULL,
   PRIMARY KEY (id_comentario),
   CONSTRAINT comentario_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO comentario VALUES('00001','00001','Pedro',32,10,'12:32','Mi opinión sobre Assassins Creed Odyssey','Es un juego muy entretenido, con mucho contenido y muchas horas de entretenimiento. La verdad es que al principio es un poco aburrido, todo hay que decirlo, pero poco a poco te va metiendo en la historia, en la trama. Tiene muchas cosas para hacer, por ejemplo (Sin hacer spoilers) hay un sistema de mercenarios, asi como una clasificación, es entretenido y te incita a ser el numero 1. Graficamente el juego es muy bonito y detallado.',12,'SI','perfil.jpg');
+INSERT INTO comentario VALUES('00001','00001',32,10,'2020/05/02 12:32','Mi opinión sobre Assassins Creed Odyssey','Es un juego muy entretenido, con mucho contenido y muchas horas de entretenimiento. La verdad es que al principio es un poco aburrido, todo hay que decirlo, pero poco a poco te va metiendo en la historia, en la trama. Tiene muchas cosas para hacer, por ejemplo (Sin hacer spoilers) hay un sistema de mercenarios, asi como una clasificación, es entretenido y te incita a ser el numero 1. Graficamente el juego es muy bonito y detallado.',1,'perfil.jpg');
 
 CREATE TABLE info_usuario_juego (
    id_info INT(10) NOT NULL,
    id_juego INT(5) NOT NULL,
    id_usuario INT(5) NOT NULL,
-   horas_juego_usuario INT(6) NOT NULL,
-   genero_jugado varchar(300) NOT NULL,
-   favorito ENUM('SI','NO') NOT NULL,
+   horas_juego_usuario TIME NOT NULL,
+   favorito BIT NOT NULL,
+   puntuado BIT NOT NULL,
    PRIMARY KEY (id_info),
    CONSTRAINT info_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
    CONSTRAINT info_juego FOREIGN KEY (id_juego) REFERENCES juego (id_juego)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO info_usuario_juego VALUES('00001','00001','00001',50,'Acción, Aventura','SI');
+INSERT INTO info_usuario_juego VALUES('00001','00001','00001','03:05:12',1,1);
+
+CREATE TABLE usuario_genero_horas (
+   id_genero INT(5) NOT NULL,
+   id_usuario INT(5) NOT NULL,
+   accion TIME,
+   aventura TIME,
+   carreras TIME,
+   casual TIME,
+   deportes TIME,
+   estrategia TIME,
+   indie TIME,
+   multijugador_masivo TIME,
+   rol TIME,
+   simuladores TIME,
+   PRIMARY KEY (id_genero),
+   CONSTRAINT usuario_genero FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO usuario_genero_horas VALUES('00001','00001','03:30:25','00:00:00','01:03:02','00:30:25','05:45:36','03:30:25','','00:12:00','01:23:01','04:15:05');
+
+CREATE TABLE juego_genero (
+   id_genero INT(5) NOT NULL,
+   id_juego INT(5) NOT NULL,
+   accion BIT NOT NULL,
+   aventura BIT NOT NULL,
+   carreras BIT NOT NULL,
+   casual BIT NOT NULL,
+   deportes BIT NOT NULL,
+   estrategia BIT NOT NULL,
+   indie BIT NOT NULL,
+   multijugador_masivo BIT NOT NULL,
+   rol BIT NOT NULL,
+   simuladores BIT NOT NULL,
+   PRIMARY KEY (id_genero),
+   CONSTRAINT juego_genero FOREIGN KEY (id_juego) REFERENCES juego (id_juego)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO juego_genero VALUES('00001','00001',1,1,0,0,0,0,0,0,1,0);
+
+CREATE TABLE juego_pegi (
+   id_pegi INT(5) NOT NULL,
+   id_juego INT(5) NOT NULL,
+   pegi_3 BIT NOT NULL,
+   pegi_7 BIT NOT NULL,
+   pegi_12 BIT NOT NULL,
+   pegi_16 BIT NOT NULL,
+   pegi_18 BIT NOT NULL,
+   violencia BIT NOT NULL,
+   mal_lenguaje BIT NOT NULL,
+   miedo BIT NOT NULL,
+   juegos_azar BIT NOT NULL,
+   sexo BIT NOT NULL,
+   drogas BIT NOT NULL,
+   discriminacion BIT NOT NULL,
+   PRIMARY KEY (id_pegi),
+   CONSTRAINT juego_pegi FOREIGN KEY (id_juego) REFERENCES juego (id_juego)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO juego_pegi VALUES('00001','00001',0,0,0,0,0,1,1,1,0,0,1,0);
