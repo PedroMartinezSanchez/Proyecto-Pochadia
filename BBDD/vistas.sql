a65de69a114034f3cfd85acc5e1f505f555114a6
Pedro <citadel153@hotmail.com>
2020-06-17 19:51:17 +0200

    Creo vista, juego_mas_jugado, juego_mejor_puntuado

---
+++ BBDD/vistas.sql
@@ -0,0 +1,12 @@
+/*Juego mejor puntuado*/
+CREATE VIEW juego_mejor_puntuado
+AS
+SELECT juego.id_juego, (puntuacion_1_S*1)+(puntuacion_2_S*2)+(puntuacion_3_S*3)+(puntuacion_4_S*4)+(puntuacion_5_S*5) as puntuacion FROM pochadia.juego
+order by puntuacion DESC;
+
+/*Juego más jugado*/
+CREATE VIEW juego_mas_jugado
+AS
+SELECT id_juego as mas_jugado, cast(sum(horas_juego_usuario) as time) as tiempo_total FROM pochadia.info_usuario_juego
+group by id_juego
+order by tiempo_total desc;
