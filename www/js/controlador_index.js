e607369a59e6ec872c6cf6acb7c549b0e324768a
Kinitx <pedro94sanchez@gmail.com>
2020-06-19 19:07:09 +0200

    subo modificaciones angular

--- www/js/controlador_index.js
+++ www/js/controlador_index.js
@@ -1,7 +1,47 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
+    $scope.slickParams = {
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '10px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    };
     $http.get("json/index_get.json")
         .then(function (response) {
             $scope.resultados = response.data.resultados;
         });
+});
+app.directive('slickSlider', function () {
+    return {
+        restrict: 'A',
+        scope: {'data': '='},
+        link: function (scope, element, attrs) {
+            var isInitialized = false;
+            scope.$watch('data', function(newVal, oldVal) {
+                if (newVal && newVal.length > 0 && !isInitialized) {
+                    $(element).slick(scope.$eval(attrs.slickSlider));
+                    isInitialized = true;
+                }
+            });
+        }
+    }
 });
