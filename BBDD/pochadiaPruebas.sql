b2dc0d8ff10115e5a4c15fe3ccbf90c3613784ff
Pedro <citadel153@hotmail.com>
2020-06-19 13:40:04 +0200

    ACtualizo la BBDD con cambios de la version 5

--- BBDD/pochadiaPruebas.sql
+++ BBDD/pochadiaPruebas.sql
@@ -41,6 +41,24 @@ INSERT INTO juego VALUES('00001','12','5','34','0','2','Assassins Creed Odyssey'
 INSERT INTO juego VALUES('00002','8','2','0','42','50','Unravel 2','Electronic Arts',2020,'NORMAL','EA Originals','Inglés, francés, italiano, alemán, español, japonés, coreano, neerlandés, polaco, portugués, ruso, chino (simplificado), chino (tradicional)','Descripción corta','Descripción larga','2020/06/17','Unravel_2.jpg',1,'NO',1);
 INSERT INTO juego VALUES('00003','3','15','1','12','60','Sea of Thieves','Rare Ltd',2020,'NORMAL','Xbox Game Studios','Inglés, francés, italiano, alemán, español, japonés, coreano, neerlandés, polaco, portugués, ruso, chino (simplificado), chino (tradicional)','Descripción corta','Descripción larga','2020/06/15','Sea_of_Thieves.jpg',1,'EN LINEA',32);
 
+
+CREATE TABLE genero (
+  id_genero INT(5) NOT NULL,
+  nombre_genero varchar(200) NOT NULL,
+  PRIMARY KEY (id_genero)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO genero VALUES('00001','Accion');
+INSERT INTO genero VALUES('00002','Aventuras');
+INSERT INTO genero VALUES('00003','Carreras');
+INSERT INTO genero VALUES('00004','Casual');
+INSERT INTO genero VALUES('00005','Deportes');
+INSERT INTO genero VALUES('00006','Estrategia');
+INSERT INTO genero VALUES('00007','Indle');
+INSERT INTO genero VALUES('00008','Multijugador masivo');
+INSERT INTO genero VALUES('00009','Rol');
+INSERT INTO genero VALUES('00010','Simuladores');
+
 CREATE TABLE comentario (
   id_comentario INT(5) NOT NULL,
   id_usuario INT(5) NOT NULL,
@@ -56,6 +74,20 @@ CREATE TABLE comentario (
 
 INSERT INTO comentario VALUES('00001','00001',32,10,'2020/05/02 12:32','Mi opinión sobre Assassins Creed Odyssey','Es un juego muy entretenido, con mucho contenido y muchas horas de entretenimiento. La verdad es que al principio es un poco aburrido, todo hay que decirlo, pero poco a poco te va metiendo en la historia, en la trama. Tiene muchas cosas para hacer, por ejemplo (Sin hacer spoilers) hay un sistema de mercenarios, asi como una clasificación, es entretenido y te incita a ser el numero 1. Graficamente el juego es muy bonito y detallado.',1);
 
+CREATE TABLE respuesta (
+  id_respuesta INT(5) NOT NULL,
+  id_comentario INT(5) NOT NULL,
+  id_usuario INT(5) NOT NULL,
+  votos_positivos INT(10) NOT NULL,
+  votos_negativos INT(10) NOT NULL,
+  hora_fecha datetime NOT NULL,
+  texto varchar(600) NOT NULL,
+  PRIMARY KEY (id_respuesta),
+  CONSTRAINT respuesta_comentario FOREIGN KEY (id_comentario) REFERENCES comentario (id_comentario),
+  CONSTRAINT respuesta_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO respuesta VALUES('00001','00001','00001',32,10,'2020/05/02 12:32','Creo que te estas equivocando ya que...');
 
 CREATE TABLE juego_carousel (
    id_carousel INT(5) NOT NULL,
@@ -85,43 +117,26 @@ INSERT INTO info_usuario_juego VALUES('00001','00001','00001','03:05:12',1,1);
 INSERT INTO info_usuario_juego VALUES('00002','00002','00001','14:50:25',1,1);
 INSERT INTO info_usuario_juego VALUES('00003','00003','00001','3:00:02',1,1);
 
-CREATE TABLE usuario_genero_horas (
-   id_genero INT(5) NOT NULL,
-   id_usuario INT(5) NOT NULL,
-   accion TIME,
-   aventura TIME,
-   carreras TIME,
-   casual TIME,
-   deportes TIME,
-   estrategia TIME,
-   indie TIME,
-   multijugador_masivo TIME,
-   rol TIME,
-   simuladores TIME,
-   PRIMARY KEY (id_genero),
-   CONSTRAINT usuario_genero FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO usuario_genero_horas VALUES('00001','00001','03:30:25','00:00:00','01:03:02','00:30:25','05:45:36','03:30:25','','00:12:00','01:23:01','04:15:05');
-
 CREATE TABLE juego_genero (
+   id_info INT(5) NOT NULL,
    id_genero INT(5) NOT NULL,
    id_juego INT(5) NOT NULL,
-   accion BIT NOT NULL,
-   aventura BIT NOT NULL,
-   carreras BIT NOT NULL,
-   casual BIT NOT NULL,
-   deportes BIT NOT NULL,
-   estrategia BIT NOT NULL,
-   indie BIT NOT NULL,
-   multijugador_masivo BIT NOT NULL,
-   rol BIT NOT NULL,
-   simuladores BIT NOT NULL,
-   PRIMARY KEY (id_genero),
-   CONSTRAINT juego_genero FOREIGN KEY (id_juego) REFERENCES juego (id_juego)
+   PRIMARY KEY (id_info),
+   CONSTRAINT info_genero FOREIGN KEY (id_genero) REFERENCES genero (id_genero),
+   CONSTRAINT info_genero_juego FOREIGN KEY (id_juego) REFERENCES juego (id_juego)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO juego_genero VALUES('00001','00001',1,1,0,0,0,0,0,0,1,0);
+INSERT INTO juego_genero VALUES('00001','00001','00001');
+INSERT INTO juego_genero VALUES('00002','00002','00001');
+INSERT INTO juego_genero VALUES('00003','00009','00001');
+INSERT INTO juego_genero VALUES('00004','00002','00002');
+INSERT INTO juego_genero VALUES('00005','00004','00002');
+INSERT INTO juego_genero VALUES('00006','00007','00002');
+INSERT INTO juego_genero VALUES('00007','00010','00003');
+INSERT INTO juego_genero VALUES('00008','00006','00003');
+INSERT INTO juego_genero VALUES('00009','00009','00003');
+INSERT INTO juego_genero VALUES('00010','00004','00003');
+
 
 CREATE TABLE juego_pegi (
    id_pegi INT(5) NOT NULL,
