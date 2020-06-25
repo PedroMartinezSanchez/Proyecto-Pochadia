55552216bcbab85d836ad9ba7b3e8a85e4536420
Manuel Generation <mgonzaleztenza@gmail.com>
2020-06-25 18:43:08 +0200

    añado info_juego.js

---
+++ www/js/info_juego.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+    $("#uno").mouseenter(function(){
+      $("#uno").css("display", "inline").fadeIn(2000);
+      $("#uno").css("color", "yellow");
+        });
+
+    $("#dos").mouseenter(function(){
+        $("#dos").css("display", "inline").fadeIn(2000);
+        $("#dos").css("color", "yellow");
+  
+        $("#uno").css("display", "inline").fadeIn(2000);
+        $("#uno").css("color", "yellow");
+      });
+
+    $("#tres").mouseenter(function(){
+        $("#tres").css("display", "inline").fadeIn(2000);
+        $("#tres").css("color", "yellow");
+  
+        $("#uno").css("display", "inline").fadeIn(2000);
+        $("#uno").css("color", "yellow");
+        $("#dos").css("display", "inline").fadeIn(2000);
+        $("#dos").css("color", "yellow");
+      });
+
+      $("#cuatro").mouseenter(function(){
+        $("#cuatro").css("display", "inline").fadeIn(2000);
+        $("#cuatro").css("color", "yellow");
+  
+        $("#uno").css("display", "inline").fadeIn(2000);
+        $("#uno").css("color", "yellow");
+        $("#dos").css("display", "inline").fadeIn(2000);
+        $("#dos").css("color", "yellow");
+        $("#tres").css("display", "inline").fadeIn(2000);
+        $("#tres").css("color", "yellow");     
+      });
+
+      $("#cinco").mouseenter(function(){
+        $("#cinco").css("display", "inline").fadeIn(2000);
+        $("#cinco").css("color", "yellow");
+  
+        $("#uno").css("display", "inline").fadeIn(2000);
+        $("#uno").css("color", "yellow");
+        $("#dos").css("display", "inline").fadeIn(2000);
+        $("#dos").css("color", "yellow");
+        $("#tres").css("display", "inline").fadeIn(2000);
+        $("#tres").css("color", "yellow");
+        $("#cuatro").css("display", "inline").fadeIn(2000);
+        $("#cuatro").css("color", "yellow");
+      });
+
+        $('#uno').mouseout(function()
+        {
+            $('#uno').css("color", "white");
+            $('#dos').css("color", "white");
+            $('#tres').css("color", "white");
+            $('#cuatro').css("color", "white");
+            $('#cinco').css("color", "white");
+        });
+
+        $('#dos').mouseout(function()
+        {
+            $('#uno').css("color", "white");
+            $('#dos').css("color", "white");
+            $('#tres').css("color", "white");
+            $('#cuatro').css("color", "white");
+            $('#cinco').css("color", "white");
+        });
+
+        $('#tres').mouseout(function()
+        {
+            $('#uno').css("color", "white");
+            $('#dos').css("color", "white");
+            $('#tres').css("color", "white");
+            $('#cuatro').css("color", "white");
+            $('#cinco').css("color", "white");
+        });
+
+        $('#cuatro').mouseout(function()
+        {
+            $('#uno').css("color", "white");
+            $('#dos').css("color", "white");
+            $('#tres').css("color", "white");
+            $('#cuatro').css("color", "white");
+            $('#cinco').css("color", "white");
+        });
+
+        $('#cinco').mouseout(function()
+        {
+            $('#uno').css("color", "white");
+            $('#dos').css("color", "white");
+            $('#tres').css("color", "white");
+            $('#cuatro').css("color", "white");
+            $('#cinco').css("color", "white");
+        });      
+  });
