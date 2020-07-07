9fa0839e74df32870f60f577668b0c1af9ce2ac8
Kinitx <pedro94sanchez@gmail.com>
2020-07-07 19:09:13 +0200

    subo perfil y controlador-perfil

--- www/js/controlador_perfil.js
+++ www/js/controlador_perfil.js
@@ -44,6 +44,16 @@ app.controller('myCtrl', function ($scope, $http, $timeout) {
     $(".img-perfil").mouseout(function(){
         $(".edit-icon").fadeOut();
     });
+    $(".edit-icon2").hide();
+    $(".img-banner").mouseover(function(){        
+        $(".edit-icon2").fadeIn();
+    });
+    $(".edit-icon2").mouseover(function(){
+        $(".edit-icon2").fadeIn();
+    });
+    $(".img-banner").mouseout(function(){
+        $(".edit-icon2").fadeOut();
+    });
     
 
     $scope.confirmar = function (event) {
@@ -65,11 +75,9 @@ app.controller('myCtrl', function ($scope, $http, $timeout) {
 
                 console.log(result);
                 if (result.isConfirmed) {
-                    //console.log("confirmado!");
-                    //console.log(reader.result);
                     $timeout(function(){
                         
-                        $scope.imagen = reader.result;
+                    $scope.imagen = reader.result;
                     }, 0);
 
                     Swal.fire(
@@ -81,35 +89,70 @@ app.controller('myCtrl', function ($scope, $http, $timeout) {
                     console.log("cancelado!");
                 }
             });
-        /*
-        reader.onload = function () {
-            Swal.fire({
-                title: "¿Estas seguro?",
-                html: "<img src='" + reader.result + "' style='width:250px;'>",
-                showCancelButton: true,
-                showClass: {
-                    popup: 'animate__animated animate__fadeInDown'
-                },
-                hideClass: {
-                    popup: 'animate__animated animate__fadeOutUp'
-                }
-            }).then(result => {
-                if (result.isConfirmed) {
-                    $timeout(function() {
-                        $scope.imagen = reader.result;
-                    }, 0);
-                    
-                } else {
-                }
-            });
-            */
         };
         reader.readAsDataURL(file);
+    };
+    $scope.confirmar2 = function (event2) {
+        var file2 = event2.target.files[0];
+        var reader2 = new FileReader();
+        
+        
+        reader2.onload = function () {
+            var image = new Image();
+            image = reader2.result;
+            image.onload = function() {
+                console.log(image.width);
+            if (image.width < 630 || image.height < 230) {
+                
+                Swal.fire({
+                    title: '¡El tamaño de la imagen es muy pequeño!',
+                    text: "Te recomendamos unas medidas de 630x230",
+                    icon: 'warning',
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Confirmar'
+                });
+            } else {
+                Swal.fire({
+                    title: '¿Estas seguro?',
+                    html: "<img src='" + reader2.result + "' style='width:300px;'>",
+                    text: "Escoge una imagen que se ajuste bien a tu gusto",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Confirmar'
+                }).then((result) => {
+    
+                    console.log(result);
+                    if (result.isConfirmed) {
+                        $timeout(function(){
+                            
+                        $scope.imagen2 = reader2.result;
+                        }, 0);
+    
+                        Swal.fire(
+                            'Imagen actualizada',
+                            '¡Ha quedado genial!',
+                            'success'
+                        )
+                    } else {
+                        console.log("cancelado!");
+                    }
+                });
+            }
+            }
+            
+            
+            
+        };
+        reader2.readAsDataURL(file2);
     };
     $http.get("json/perfil_get_" + $scope.idUsuario + ".json")
         .then(function (response) {
             $scope.resultados = response.data.resultados;
             $scope.imagen = response.data.resultados[0].img_perfil;
+            $scope.imagen2 = response.data.resultados[0].img_banner;
         });
 });
 
